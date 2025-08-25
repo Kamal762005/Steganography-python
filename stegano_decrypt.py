702da015296ca331626c0ca607381e0570b1a6d8
kamal chalumuri <158756028+Kamal762005@users.noreply.github.com>
2025-08-25 08:54:54 +0530

    Update stegano_decrypt.py

--- stegano_decrypt.py
+++ stegano_decrypt.py
@@ -1,36 +1,53 @@
 import cv2
 import string
+import AES
 
 def decrypt(c,d):
     path=input("Enter Path Of Image To Decrypt: ")
     path = path.replace("\"",'')
     img1 = cv2.imread(path)
-
-    #img1="/img1.jpg"
+    
+    # calculating height and width.
+    h,w=img1.shape[:2]
+        
     # Retriving the password from the image.
     password = ""
-    n , m = img1.shape[:2]
     z = 0
-    for i in range(255):
-        if(c[img1[n-1,m-1,z]]=='!'):
-            break
-        password=password + c[img1[n-1,m-1,z]]
-        n = n - 1
-        m = m - 1
-        z = (z + 1) % 3
+    for col in range(w):
+        value = img1[h - 1, col, z]
+        if value in c:
+            char = c[value]
+            if char == '!':
+                break
+            password += char
+        else:
+            print(f"Invalid value in password section: {value}")
+            return
     
     # Retriving the data from the image.
     password1 = input("Enter Password For Decryption: ")
     if (password1 == password):
-        msg = ""
-        m, n, z = 0, 0, 0
-        for i in range(255):
-            if(c[img1[n,m,z]]=='$'):
+        print("YOU ARE AUTHENTICATED.")
+        ct = ""
+        stop = False
+        for i in range(h):
+            for j in range(w):
+                for k in range(3):
+                    value = img1[i, j, k]
+                    if value in c:
+                        char = c[value]
+                        if char == '$':
+                            stop = True
+                            break
+                        ct += char
+                if stop:
+                    break
+            if stop:
                 break
-            msg=msg + c[img1[n,m,z]]
-            n = n + 1
-            m = m + 1
-            z = (z + 1) % 3
+
+        # decrypting the cipher text obtained from image
+        msg = AES.aes_decrypt(ct,password)
         print("Decrypted message: ",msg)
     else:
         print("YOU ARE NOT AUTHENTICATED!")
+
