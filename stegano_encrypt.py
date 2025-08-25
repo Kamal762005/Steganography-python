1a9d50e4e082701b586a08412639cf7773f4f295
kamal chalumuri <158756028+Kamal762005@users.noreply.github.com>
2025-08-25 08:55:32 +0530

    Update stegano_encrypt.py

--- stegano_encrypt.py
+++ stegano_encrypt.py
@@ -1,27 +1,42 @@
 import cv2
 import string
+import AES 
 
 def encrypt(input_text,c,d,p):
     path=input("Enter Path Of Image To Encrypt: ")
     path = path.replace("\"",'')
     img1 = cv2.imread(path)
 
-    #img1="/img1.jpg"
-    # storing password into an image
-    n,m=img1.shape[:2]
-    z=0
-    for i in range(len(p)):
-        img1[n-1,m-1,z] = d[p[i]]
-        n = n - 1
-        m = m - 1
-        z = (z + 1) % 3
+    # calculating height and width.
+    h,w=img1.shape[:2]
     
-    # storing data into an image
-    m, n, z = 0, 0, 0
-    for i in range(len(input_text)):
-        img1[n,m,z] = d[input_text[i]]
-        n = n + 1
-        m = m + 1
-        z = (z + 1) % 3
+    # using aes algorithm, we can encrypt text and embedded into a image.
+    # $ - indicates end of cipher text.
+    ct = AES.aes_encrypt(input_text,p)+'$'
+    
+    # check for maximum capacity of an image to store the text.
+    max=h*w*3
+    if len(ct) > max:
+        print("Error: Input text is too long for the selected image.")
+
+    # storing data into an image using 3 channels(3 characters are stored in a single pixel).
+    v = 0
+    for i in range(h):
+        for j in range(w):
+            for k in range(3):
+                if v < len(ct):
+                    img1[i, j, k]=d[ct[v]]
+                    v += 1
+                else:break
+            if(v>len(ct)):break
+        if(v>len(ct)):break
+
+    # storing password into an image only using lsb bit of last row.
+    # ! - indicates end of password.
+    n=h-1
+    z=0
+    p=p+'!'
+    for m in range(min(len(p),w)):
+        img1[n,m,z] = d[p[m]]
     img1 = cv2.imwrite("encrypted_image.png",img1)
     return img1
